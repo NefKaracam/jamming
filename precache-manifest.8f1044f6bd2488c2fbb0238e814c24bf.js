0f19cf8d5cc243fe9aaa25494980decdcab41a33
Nefise Karacam <nefise.cekdemir@gmail.com>
2021-05-23 21:36:22 +0700

    Updates

---
+++ precache-manifest.8f1044f6bd2488c2fbb0238e814c24bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65f2a159aa4e74448ce6326a145cf21",
+    "url": "/jamming/index.html"
+  },
+  {
+    "revision": "9675a827e93c3269eb2d",
+    "url": "/jamming/static/css/main.2de96377.chunk.css"
+  },
+  {
+    "revision": "507917f4948898a31153",
+    "url": "/jamming/static/js/2.78dc8e51.chunk.js"
+  },
+  {
+    "revision": "9675a827e93c3269eb2d",
+    "url": "/jamming/static/js/main.44f1c883.chunk.js"
+  },
+  {
+    "revision": "c3d7dfc925f613b07d70",
+    "url": "/jamming/static/js/runtime~main.6e9a7a58.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jamming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
